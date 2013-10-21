539ad90fb90c56216205dddaf83109a9b3ba8612
Yuan Xia <yuan.xia.cpp@gmail.com>
2013-10-21 14:43:06 -0400

    initial chckin

---
+++ project2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+void dumb_solve(double *a, double *y, int n, double eps,
+		int numit, double *x, int *niter, double *discreps) {
+  
+}
+
+int main() {
+  
+}
